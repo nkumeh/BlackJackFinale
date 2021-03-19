c942197707a920574934844cc491f5abe9304118
Maria Piper <mbtomori@users.noreply.github.com>
2021-03-18 21:05:02 -0700

    Add hand class and tests

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 
 /**
  * This class represents a card hand. It inherits from the deck class.
+ *
+ * This class utilizes the methods in the parent class.
+ *
+ * This class includes two constructors:
+ *     The no-argument constructor creates an empty hand with a maxHandSize initialized to zero.
+ *     The constructor that allows you to set the maxHandSize.
  */
 
 public class Hand extends Deck {
@@ -21,20 +27,85 @@ public class Hand extends Deck {
      */
     public Hand(int maxCards) {
         super(new ArrayList<>());
+        setHandSize(maxCards);
     }
 
-    public void setHandSize(int newHandSize) {
-        if (isValidHandSize(newHandSize)) {
+    /**
+     * Getter method for the maxHandSize, the most cards one can add to a hand.
+     * @return maxHandSize (an int).
+     */
+    public int getHandSize() {
+        return this.maxHandSize;
+    }
+
+    /**
+     * This method allows you to set the maximum number of cards in a hand.
+     * @param newHandSize an integer
+     * @throws IllegalArgumentException if the maximum number of cards is less than 1 or greater than
+     *                                  the maximum number of cards in a deck.
+     */
+    public void setHandSize(int newHandSize) throws IllegalArgumentException {
+        if (newHandSize > 0 && newHandSize <= super.MAX_CARDS_IN_DECK) {
             this.maxHandSize = newHandSize;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid Hand Size: " + newHandSize);
         }
     }
 
-    public static void main(String[] args) {
-        Hand myHand = new Hand();
-        System.out.println(myHand.size());
+    @Override
+    boolean isFull() {
+        return this.size() >= maxHandSize;
     }
 
-    private boolean isValidHandSize(int handSize) {
-        return handSize > 0 && handSize <= super.MAX_CARDS_IN_DECK;
+
+    public static void main(String[] args) {
+        // Create and shuffle a new deck.
+        Deck myLuckyDeck = new Deck();
+        myLuckyDeck.shuffle();
+
+        // Create two hands with 5 cards each. The assert statements
+        // check that the card has been removed from the deck.
+        int handSize = 5;
+        Hand myHand = new Hand(handSize);
+        Hand yourHand = new Hand(handSize);
+        for (int i = 0; i < handSize; i++) {
+            myHand.add(myLuckyDeck.takeTopCard());
+            assert(!myLuckyDeck.hasCard(myHand.getCard(i)));
+            assert(myHand.size() == i + 1);
+
+            yourHand.add(myLuckyDeck.takeTopCard());
+            assert(!myLuckyDeck.hasCard(yourHand.getCard(i)));
+            assert(yourHand.size() == i + 1);
+        }
+        assert(myLuckyDeck.size() == 42);
+
+        // Prints the hands and the Decks
+        myHand.printDeck();
+        yourHand.printDeck();
+        myLuckyDeck.printDeck();
+
+        // Returns the cards to the deck. The assert statements
+        // check that the cards are no longer a part of the hand
+        // and that they are in the deck
+        for (int i = 0; i < handSize; i++) {
+            Card myTopCard = myHand.takeTopCard();
+            assert(!myLuckyDeck.hasCard(myTopCard));
+            myLuckyDeck.add(myTopCard);
+            assert(myLuckyDeck.hasCard(myTopCard));
+            assert(!myHand.hasCard(myTopCard));
+
+            Card yourTopCard = yourHand.takeTopCard();
+            assert(!myLuckyDeck.hasCard(yourTopCard));
+            myLuckyDeck.add(yourTopCard);
+            assert(myLuckyDeck.hasCard(yourTopCard));
+            assert(!yourHand.hasCard(yourTopCard));
+        }
+        assert(myLuckyDeck.size() == 52);
+        myHand.printDeck();
+        yourHand.printDeck();
+        myLuckyDeck.printDeck();
+
+        //Sort is being tested in the HandTest.java file.
     }
 }
